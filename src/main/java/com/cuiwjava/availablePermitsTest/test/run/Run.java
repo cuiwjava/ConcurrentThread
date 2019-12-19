fad14e70c7ed827884d8e7de019cce9958fec2cf
cuiwei34 <cuiwei34@jd.com>
2019-12-19 09:43:21 +0800

    log

---
+++ src/main/java/com/cuiwjava/availablePermitsTest/test/run/Run.java
@@ -0,0 +1,13 @@
+package com.cuiwjava.availablePermitsTest.test.run;
+
+import com.cuiwjava.availablePermitsTest.myservice.MyService;
+
+public class Run {
+
+	public static void main(String[] args) {
+		MyService service = new MyService();
+		service.testMethod();
+
+	}
+
+}
---
+++ src/main/java/com/cuiwjava/drainPermitsTest/test/run/Run.java
@@ -0,0 +1,13 @@
+package com.cuiwjava.drainPermitsTest.test.run;
+
+import com.cuiwjava.drainPermitsTest.myservice.MyService;
+
+public class Run {
+
+	public static void main(String[] args) {
+		MyService service = new MyService();
+		service.testMethod();
+
+	}
+
+}
